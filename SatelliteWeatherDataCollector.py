86843b5f2420ecf6dfbab64cf3d1cdc478b915f6
Anna Henderson <amh343@cornell.edu>
2021-12-24 16:48:33 -0500

    add docstrings and clean names

--- SatelliteWeatherDataCollector.py
+++ SatelliteWeatherDataCollector.py
@@ -10,7 +10,9 @@ from requests.api import request
 
 
 def parse_args():
-    # parse arguments
+    """Parse and return the latitude and longitude arguments. 
+    Used for testing in command line.
+    """
     parser = argparse.ArgumentParser(
         description='Outputs satellite data for the given latitude and longitude')
     parser.add_argument("--lat", help="latitude", default="0")
@@ -20,7 +22,14 @@ def parse_args():
 
 
 def scrape(lat, long):
-    # scrape site
+    """Scrape html from heavens above ISS result site 
+
+    Parameters:
+    lat (str): latitude
+    long (str): longitude
+
+    Returns BeautiulSoup object containing html
+    """
     # TODO: Allow user to input desired satellite, timezome, and altitude
     url = "https://www.heavens-above.com/PassSummary.aspx?satid=25544&lat=" + \
         lat+"&lng="+long+"&loc=Unnamed&alt=0&tz=EST"
@@ -28,7 +37,16 @@ def scrape(lat, long):
     return bs4.BeautifulSoup(page.content, 'lxml')
 
 
-def parse_data(soup, col_names, indices):
+def parse_satellite_data(soup, col_names, indices):
+    """Extract and store satellite data from scraped html [soup].
+
+    Parameters:
+    indices: iterable specifing which columns from site table are desired
+    col_names: contains column names for returned dataframe
+
+    Returns:
+    pandas dataframe containing data with columns labeled by col_names
+    """
     data = pd.DataFrame(columns=col_names)
     rows = soup.find_all('tr', attrs={'class': 'clickableRow'})
     # Parse data from page
@@ -38,24 +56,45 @@ def parse_data(soup, col_names, indices):
         for i in indices:
             entry.append(row_data[i].text)
         data = data.append(pd.DataFrame([entry], columns=col_names))
-
-    # convert to html
     return data
 
 
-# Must specify which columns in heavens above table to grab. The columns are in the following order:
-# "Date", "Brightness", "Start Time", "Start Alt.", "Start Az.", "Hightest Pt. Time", "Hightest Pt. Alt.", "Hightest Pt. Az.", "End Time", "End Alt.", "End Az.", "Pass type"
-def getSatelliteData(lat, long):
+def get_satellite_data(lat, long):
+    """
+    Collect and return satellite data from heavens above
+
+    Retrieves date, start time, and end time data for each satellite pass
+
+    Parameters:
+    lat (str): latitude
+    long (str): longitude
+
+    Returns:
+    pandas dataframe containing satellite data for next 10 days
+    """
     soup = scrape(lat, long)
-    # create data frame
     indices = [0, 2, 8]
     names = ("Date", "Start time", "End time")
-    return parse_data(soup, names, indices)
+    return parse_satellite_data(soup, names, indices)
 
     # TODO: Allow users to request specific information about the satellite rather than/ in addition to just printing a table
 
 
-def getWeatherData(lat, long, test):
+def get_weather_data(lat, long, test):
+    """
+    Collect and return weather information using tomorrow.io API
+
+    Collects data about temperature, cloud cover, preciption intensity,  
+    and precipitation type for next 15 days
+
+    Parameters:
+    lat (str): latitude
+    long (str): longitude
+    test (boolean): When false, sends API call. When true, use saved API response
+
+    Returns:
+    pandas dataframe containing weather data for next 10 days 
+    """
     if (not test):
         url = "https://api.tomorrow.io/v4/timelines?location="+long+","+lat + \
             "&fields=temperature,cloudCover,precipitationIntensity,precipitationType,&timesteps=1d&units=metric&apikey=" + \
@@ -74,34 +113,40 @@ def getWeatherData(lat, long, test):
     return result
 
 
+# Used to find length of month for mergeData()
 month_end = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 
-def mergeData(satData, weatherData):
-    result = satData
-    first_weather_time = weatherData.loc[0, "weather date"]
-    startday = int(first_weather_time[8:10])
+def merge_data(sat_data, weather_data):
+    """Merge satellite data and weather data into a single table.
+
+    For each entry in sat_data, find the corresponding entry in weather_data and 
+    concatenate it to that row (prioritize satellite data over weather data).
+    """
+    result = sat_data
+    first_weather_time = weather_data.loc[0, "weather date"]
+    start_day = int(first_weather_time[8:10])
     month = int(first_weather_time[5:7])
     end_month = month_end[month]
     concat_weather = pd.DataFrame()
-    for i in range(len(satData)):
-        day = int(satData.loc[i, "Date"][0:2])
-        index = day - startday if (day-startday >
-                                   0) else end_month-startday + day
-        concat_weather = concat_weather.append(weatherData.iloc[index])
+    for i in range(len(sat_data)):
+        day = int(sat_data.loc[i, "Date"][0:2])
+        index = day - start_day if (day-start_day >
+                                    0) else end_month-start_day + day
+        concat_weather = concat_weather.append(weather_data.iloc[index])
     concat_weather = concat_weather.reset_index(drop=True)
-    result = pd.concat((satData, concat_weather), axis=1)
+    result = pd.concat((sat_data, concat_weather), axis=1)
     return result
 
 
-def getData(lat, long):
-    sat_data = getSatelliteData(lat, long)
+def get_data(lat, long):
+    """Fetch satellite and weather data and return html table of results."""
+    sat_data = get_satellite_data(lat, long)
     sat_data = sat_data.reset_index(drop=True)
-    weather_data = getWeatherData(lat, long, test=True)
-    result = mergeData(sat_data, weather_data)
+    weather_data = get_weather_data(lat, long, test=True)
+    result = merge_data(sat_data, weather_data)
     return result.to_html(index=False)
 
 
 if __name__ == "__main__":
-    print("hi")
-    getData("41", "-74")
+    get_data("41", "-74")
--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def table():
-    return render_template("index.html", data=SatelliteWeatherDataCollector.getSatelliteData("0", "0"))
+    return render_template("index.html", data=SatelliteWeatherDataCollector.get_satellite_data("0", "0"))
 
 
 @app.route("/", methods=["POST"])
 def update_table():
     latitude = request.form["latitude"]
     longitude = request.form["longitude"]
-    return render_template("index.html", data=SatelliteWeatherDataCollector.getData(latitude, longitude))
+    return render_template("index.html", data=SatelliteWeatherDataCollector.get_data(latitude, longitude))
